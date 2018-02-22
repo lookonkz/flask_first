5e46ca46cb1b4d679c6e6adf4e1dfc24dac5ae2f
user1 <user1@iMac-user1.local>
2018-02-22 17:27:55 +0600

    first flask

---
+++ project.py
@@ -0,0 +1,75 @@
+from flask import Flask, request, make_response, abort, render_template, session, redirect, url_for, flash
+from datetime import datetime
+from flask.ext.bootstrap import Bootstrap
+from flask.ext.moment import Moment
+from forms import NameForm
+import sqlite3
+# from flask.ext.script import Manager
+from flask.ext.sqlalchemy import SQLAlchemy
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'app.db')
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+db = SQLAlchemy(app)
+app.config['SECRET_KEY'] = '1b2i28bmqznh3uuk9b4rgz21tslch9492v6e3zdier73sj6wl0'
+bootstrap = Bootstrap(app)
+moment = Moment(app)
+
+
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    users = db.relationship('User', backref='role')
+
+    def __repr__(self):
+        return '<Role %r>' % self.name
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+# manager = Manager(app)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        old_name = session.get('name')
+        if old_name is not None and old_name != form.name.data:
+            flash('Похоже, вы изменили свое имя!')
+        session['name'] = form.name.data
+        form.name.data = ''
+        return redirect(url_for('index'))
+    return render_template('index.html', form=form, name=session.get('name'), current_time=datetime.utcnow())
+
+
+@app.route('/user')
+def user():
+    user_agent = request.headers.get('user-agent')
+    return render_template('user.html', name=user_agent)
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
+
+
+if __name__ == '__main__':
+    app.run()
